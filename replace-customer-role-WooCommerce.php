7c666d8419aec8ee0f6f33cc4a9a813270734925
Ugur Terzi <ugurterzi@triceps.com.tr>
2020-11-24 15:00:00 -0500

    replace-customer-role-WooCommerce.php
    Replace the 'customer' role (WooCommerce default) with your own one.

---
+++ replace-customer-role-WooCommerce.php
@@ -0,0 +1,11 @@
+/**
+ * Replace 'customer' role (WooCommerce default) with your own one.
+**/
+
+add_filter('woocommerce_new_customer_data', 'wc_assign_custom_role', 10, 1);
+
+function wc_assign_custom_role($args) {
+  $args['role'] = 'subscriber';
+  
+  return $args;
+}
