cd3a67f03eaa45ea5889b5314f0dbace70c2c6f5
Ugur Terzi <ugurterzi@triceps.com.tr>
2021-02-18 11:54:19 -0500

    Update last-order-class-to-body.php

--- last-order-class-to-body.php
+++ last-order-class-to-body.php
@@ -2,6 +2,13 @@ add_filter( 'body_class', 'order_class');
 
 function order_class( $orderclasses ) {
 
+    // bail if Memberships isn't active
+    if ( ! function_exists( 'wc_memberships' ) ) {
+                return;
+        }
+        
+    if ( wc_memberships_is_user_active_member( $user_id, 'membership-name' ) ) {
+
     $user_id = get_current_user_id(); // The current user ID
     
     // Get the WC_Customer instance Object for the current user
@@ -14,8 +21,9 @@ function order_class( $orderclasses ) {
     $order_data   = $last_order->get_data(); // Get the order unprotected data in an array
     $order_status = $last_order->get_status(); // Get the order status
     
-    if( is_page( 30 ))  {
+    if (is_account_page()) {
         $orderclasses[] = $order_status;
-    }
-    return $orderclasses;
+    	}
+    	return $orderclasses;
+	}
 }
